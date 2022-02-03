7bfed6fc3ba284c365647a7caff9dbd24e516846
Sterge <sv819@ic.ac.uk>
2022-02-03 18:48:55 +0000

    Complete CW 1

--- src/cw1.lean
+++ src/cw1.lean
@@ -151,7 +151,11 @@ begin
 end
 
 def peak (a : ℕ → ℝ) (i : ℕ) : Prop:=
-  ∀(n : ℕ), n > i → a n > a i
+  ∀(n : ℕ), n > i → a n < a i
+
+def recursive (f : ℕ → ℕ) (i : ℕ) : ℕ → ℕ
+| 0       :=  f i
+| (x + 1) :=  f (recursive x)
 
 
 theorem bolzano_weierstrass {a : ℕ → ℝ} (hb: ∃(M:ℝ), ∀(n: ℕ), |a n| ≤ M) :
@@ -159,8 +163,134 @@ theorem bolzano_weierstrass {a : ℕ → ℝ} (hb: ∃(M:ℝ), ∀(n: ℕ), |a n
 begin
   by_cases (∀(t : ℕ), ∃(u: ℕ), u > t ∧ peak a u),
   {
-    sorry,
+    choose r hr using h,
+    let s := recursive r 0,
+    have hinc : strictly_increasing s,
+    {
+      intro n,
+      specialize hr (s n),
+      cases hr with hi,
+      exact hi,
+    },
+    --Prove that every point in the subsequence is a peak.
+    --The hypothesis hr is not quite what we need since given s(n) it tells us that s(n+1) is a peak.
+    have hpeak: ∀(n : ℕ), peak a (s n),{ 
+      intro n,
+      by_cases n = 0,
+      rw h,
+      change peak a (r 0),
+      specialize hr 0,
+      cases hr,
+      exact hr_right,
+      have h2 : ∃ (c : ℕ), n = c.succ,
+      apply nat.exists_eq_succ_of_ne_zero,
+      exact h,
+      cases h2,
+      rw h2_h,
+      change peak a (r (s h2_w)),
+      specialize hr (s h2_w),
+      cases hr,
+      exact hr_right,
+    },
+    have hsmi : monotone_decreasing (λ t, a (s t)),
+    {
+      intro n,
+      change a ( s ( n + 1)) ≤ a ( s n ),
+      specialize hpeak n,
+      specialize hpeak (s (n + 1)),
+      specialize hinc n,
+      have g: a (s (n + 1)) < a (s n),
+      apply hpeak, exact hinc,
+      linarith,
+    },
+    use s,
+    split,
+    exact hinc,
+    apply bdd_dec_seq_converges,
+    cases hb,
+    use -hb_w,
+    intro n,
+    specialize hb_h (s n),
+    exact neg_le_of_abs_le hb_h,
+    exact hsmi,
   },{
-    sorry,
+    have h2: ∃ (p : ℕ), ∀ (q : ℕ), p < q → (∃ (n : ℕ), q < n ∧ a q ≤ a n),
+    --Some logic to get this into a form that is easier to work with.
+    {
+      rw not_forall at h, cases h,
+      use h_w, rw not_exists at h_h,
+      intro q,
+      specialize h_h q,
+      rw not_and at h_h,
+      intro hnq,
+      have hp: ¬peak a q,
+      apply h_h, exact hnq,
+      change ¬(∀(n : ℕ), n > q → a n < a q) at hp,
+      rw not_forall at hp,
+      cases hp,
+      use hp_w,
+      rw ←not_lt,
+      rw ←not_le,
+      rw ←not_or_distrib,
+      rw ←not_lt,
+      rw gt_iff_lt at hp_h,
+      rw ←imp_iff_not_or,
+      exact hp_h
+    },
+    --More tinkering so that the hypothesis works for choose.
+    cases h2 with p h3,
+    have h4: ∀ (r : ℕ), ∃ (n : ℕ), r < n ∧ a (r + (p+1)) ≤ a (n+ (p+1)),
+    intro r, specialize h3 (r + (p+1)),
+    have h5: (∃ (n : ℕ), r + (p+1) < n ∧ a (r + (p+1)) ≤ a n),
+    apply h3,
+    rw add_comm,
+    rw add_assoc,
+    rw lt_add_iff_pos_right,
+    exact nat.zero_lt_one_add r,
+    cases h5,
+    cases h5_h,
+    use h5_w - (p + 1),
+    split,
+    rw lt_tsub_iff_right,
+    exact h5_h_left,
+    have h6: h5_w - (p + 1) + (p + 1) = h5_w,
+    apply nat.sub_add_cancel,
+    linarith [h5_h_left],
+    rw h6,
+    exact h5_h_right,
+    -- Construct the subsequence
+    choose b hbs using h4,
+    let c := recursive b 0,
+    let s := (λ n, c n + (p + 1)),
+    
+    have hinc : strictly_increasing s,
+    {
+      intro n,
+      specialize hbs (c n),
+      cases hbs,
+      change s n < s (n + 1),
+      rw add_lt_add_iff_right,
+      exact hbs_left,
+    },
+    -- Prove that it's monotone to use the theorem at the start
+    have hmi : monotone_increasing (λ n, a (s n)),
+    {
+      intro n,
+      change a (c n + (p + 1)) ≤ a (c (n + 1) + (p + 1)),
+      specialize hbs (c n),
+      cases hbs,
+      exact hbs_right,
+    },
+    use s,
+    split,
+    exact hinc,
+    apply bdd_inc_seq_converges,
+    cases hb,
+    use hb_w,
+    intro n,
+    specialize hb_h (s n),
+    apply le_of_abs_le,
+    exact hb_h,
+    exact hmi,
   }
 end 
